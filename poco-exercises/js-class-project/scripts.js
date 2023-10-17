ce52222d7e6a6ed33b56c36376960c003d49d5b6
Meleknur <meleknur.ozgu@powercoders.org>
2023-10-17 12:32:30 +0200

    add details in the article

--- poco-exercises/js-class-project/scripts.js
+++ poco-exercises/js-class-project/scripts.js
@@ -1,65 +1,64 @@
-
-
 const detailsOfPeople = [
-    {
-        fname: "Faranak",
-        lname: "Rahimi",
-        eyeColor: "braun"
-    },
+  {
+      fname: "Faranak",
+      lname: "Rahimi",
+      eyeColor: "braun"
+  },
 
-    {
-        fname: "Ali",
-        lname: "Dinc",
-        eyeColor: "braun"
-    },
-    {
-        fname: "Gebre",
-        lname: "Kahsay",
-        eyeColor: "braun"
-    }
+  {
+      fname: "Ali",
+      lname: "Dinc",
+      eyeColor: "braun"
+  },
+  {
+      fname: "Gebre",
+      lname: "Kahsay",
+      eyeColor: "braun"
+  }
 ];
 
 
-
 function giveDetails(articleId) {
+  const container = document.getElementById(`details-container-${articleId}`);
+  container.innerHTML = ""; 
 
-    detailsOfPeople.forEach(function(element) {
-    
-        if (element.fname.toLowerCase() === articleId.toLowerCase()) {
-            console.log(element);
+  detailsOfPeople.forEach(function (element) {
+    if (element.fname.toLowerCase() === articleId.toLowerCase()) {
+      const ul = document.createElement("ul");
 
-            const detailsDiv = document.createElement("div");
+      const firstNameItem = document.createElement("li");
+      firstNameItem.textContent = `First Name: ${element.fname}`;
+      ul.appendChild(firstNameItem);
 
-            detailsDiv.textContent = `First Name: ${element.fname}, Last Name: ${element.lname}, Eye Color: ${element.eyeColor}`;
+      const lastNameItem = document.createElement("li");
+      lastNameItem.textContent = `Last Name: ${element.lname}`;
+      ul.appendChild(lastNameItem);
 
-            
-            const article = document.getElementById(articleId);
+      const eyeColorItem = document.createElement("li");
+      eyeColorItem.textContent = `Eye Color: ${element.eyeColor}`;
+      ul.appendChild(eyeColorItem);
 
-           
-            article.appendChild(detailsDiv);
-
-
-            //1. use document.createElement to create an html tag
-            //2. use textContent to add value of 1 specific element property to tag
-            //3. append element to container in DOM 
-
-
-
-        }
-    });
-
+      container.appendChild(ul);
+    }
+  });
 }
 
 
+ // Clear the details when the mouse leaves
+ document.querySelectorAll(".individual").forEach(function (article) {
+  article.addEventListener("mouseleave", function () {
+    const articleId = article.id;
+    const container = document.getElementById(`details-container-${articleId}`);
+    container.innerHTML = "";
+  });
+});
 
-//var item = document.getElementById("faranak");
-//item.addEventListener("mouseover", )
 
 
 
 /*
-if when I hoverover of an image, id of the element, can match the id of description list, 
-display whole description of the person
+ when I hoverover of an image, id of the element, can match the id of description list, 
+and display whole description of the person
 
 
 */
