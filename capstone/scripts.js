b1bc5e9761d56e0b3af6d69472d39d58ce8a0481
Meleknur <meleknur.ozgu@powercoders.org>
2023-11-02 15:07:03 +0100

    add eventlistener for hover over function on flag

--- capstone/scripts.js
+++ capstone/scripts.js
@@ -1,1 +1,12 @@
 
+const mountain= document.getElementById("mountain");
+const hoverArea = document.getElementById('hover-area');
+
+
+hoverArea.addEventListener('mouseover', function () {
+    mountain.src = '/images/mountain-hover.png';
+});
+
+hoverArea.addEventListener('mouseout', function () {
+    mountain.src = '/images/mountain-default.png';
+});
