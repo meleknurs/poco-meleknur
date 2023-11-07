ed8b229fb1961e04cf019e676ec6854825e7cbfd
Meleknur <meleknur.ozgu@powercoders.org>
2023-11-07 15:47:59 +0100

    add more js for image effect when hoverover

--- capstone/scripts.js
+++ capstone/scripts.js
@@ -1,22 +1,50 @@
 const mountainImage = document.getElementById("mountain");
-const hoverArea = document.getElementById('hover-area');
+const hoverArea = document.getElementById("hover-area");
 const pixelMeImage = document.getElementById("pixel-me");
 const helloTextImage = document.getElementById("hello-text");
+const testImage = document.getElementById("test-image"); 
+const codingImage = document.getElementById("coding-image"); 
+const uxImage = document.getElementById("ux-image"); 
 
-hoverArea.addEventListener('mouseover', function () {
-    mountainImage.src = '/images/mountain-hover.png';
+uxImage.addEventListener("mouseover", function () {
+    uxImage.src =  "/images/ux-image-fire.png"
+});
+  
+  uxImage.addEventListener("mouseout", function () {
+    uxImage.src = "/images/ux-image.jpg" 
 });
 
-hoverArea.addEventListener('mouseout', function () {
-    mountainImage.src = '/images/mountain-default.png';
+testImage.addEventListener("mouseover", function () {
+  testImage.src =  "/images/test-image-fire.png";
+});
+
+testImage.addEventListener("mouseout", function () {
+  testImage.src = "/images/test-image.jpg"; 
+});
+
+codingImage.addEventListener("mouseover", function () {
+    codingImage.src = "/images/code-image-fire.png"
+});
+  
+codingImage.addEventListener("mouseout", function () {
+    codingImage.src = "/images/code-image.jpg";
+});
+
+hoverArea.addEventListener("mouseover", function () {
+  mountainImage.src = "/images/mountain-hover.png";
+});
+
+hoverArea.addEventListener("mouseout", function () {
+  mountainImage.src = "/images/mountain-default.png";
 });
 
 
-pixelMeImage.addEventListener('click', function(){
-    if (helloTextImage.style.display === 'none') {
-        helloTextImage.style.display = 'block';
-    } else {
-        helloTextImage.style.display = 'none';
-    }
+pixelMeImage.addEventListener("click", function () {
+  if (helloTextImage.style.display === "none") {
+    helloTextImage.style.display = "block";
+  } else {
+    helloTextImage.style.display = "none";
+  }
 });
 
+document.getElementById("currentYear").textContent = new Date().getFullYear();
